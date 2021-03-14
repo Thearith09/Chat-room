998eb6e58943753b8453ae75d6db00ce850588bf
Thearith09 <puthearith0909@gmail.com>
2021-03-14 07:05:47 +0700

    added firebase/config.js and main.css

---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAfQPFZ2N5nyMWhq8a2LorZRjWclfWzV6E",
+    authDomain: "vue-firebase-sites-e7ad6.firebaseapp.com",
+    projectId: "vue-firebase-sites-e7ad6",
+    storageBucket: "vue-firebase-sites-e7ad6.appspot.com",
+    messagingSenderId: "711542868237",
+    appId: "1:711542868237:web:c1717cf393e6fb72bb4844"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  const projectFirestore = firebase.firestore();
+  const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+  export { projectfirestore, timestamp };
